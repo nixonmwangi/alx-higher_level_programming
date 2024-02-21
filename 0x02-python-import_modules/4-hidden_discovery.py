da9c345950394d92516edd93beef574ce8f255f4
nixonmwangi <nixonmwangi4870@gmail.com>
2024-02-21 04:52:03 -0800

    python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -9,15 +9,4 @@ def print_hidden():
             print(i)
 
 if __name__ == "__main__":
-    print_hidden()#!/usr/bin/python3.4
-import hidden_4
-
-
-def print_hidden():
-    hidden = dir(hidden_4)
-    for i in hidden:
-        if(i[:2] != "__"):
-            print(i)
-
-if __name__ == "__main__":
     print_hidden()
