5443a73285aef3cb00bf8c499cec0a7d45d71b0e
nixonmwangi <nixonmwangi4870@gmail.com>
2024-02-21 02:18:08 -0800

    python

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 from add_0 import add
 
-if __name__ == "__main__":
-    from add_0 import add
 
+def sum():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    sum()
