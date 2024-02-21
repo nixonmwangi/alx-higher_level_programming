ec12715c252af843e224898405f1508722fdba6c
nixonmwangi <nixonmwangi4870@gmail.com>
2024-02-21 03:59:03 -0800

    python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+import hidden_4
+
+
+def print_hidden():
+    hidden = dir(hidden_4)
+    for i in hidden:
+        if(i[:2] != "__"):
+            print(i)
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
+    print_hidden()#!/usr/bin/python3.4
+import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+
+def print_hidden():
+    hidden = dir(hidden_4)
+    for i in hidden:
+        if(i[:2] != "__"):
+            print(i)
+
+if __name__ == "__main__":
+    print_hidden()
